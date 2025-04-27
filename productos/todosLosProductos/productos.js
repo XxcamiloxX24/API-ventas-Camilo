c6659d3095cc1cb0b706d96db0bd29275c528de4
XxcamilolxX24 <camilovillalobos252@gmail.com>
2025-04-26 23:02:59 -0500

    no recargar pag

--- productos/todosLosProductos/productos.js
+++ productos/todosLosProductos/productos.js
@@ -78,10 +78,10 @@ function eliminarProd(id) {
           })
           .then(() => {
               document.getElementById("allProd").innerHTML = "";
-              location.reload();})
+              getItems()})
           .catch(error => console.error('Unable to delete item.', error));
     } else {
-        location.reload();
+        getItems();
     }
      
 }
